01974a42069961d198390f67ffc6ba1902879f61
seanwestfall <sean.westfall@gmail.com>
2020-04-28 03:59:42 -0700

    sw / center everything

--- src/index.html.jsx
+++ src/index.html.jsx
@@ -2,9 +2,9 @@ import About from './pages/about.html.mdx'
 
 export default () => {
   return (
-    <>
+    <main style={{textAlign: 'center'}}>
       <h1>Monday Morning Surf Club!</h1>
       <About />
-    </>
+    </main>
   )
 }
