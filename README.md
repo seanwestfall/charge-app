680313a37c1d2992bce363984af6f571631b95c0
seanwestfall <sean.westfall@gmail.com>
2020-04-27 20:44:45 -0700

    changed the title of the page

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # charge-app
+
+  
--- src/index.html.jsx
+++ src/index.html.jsx
@@ -3,7 +3,7 @@ import About from './pages/about.html.mdx'
 export default () => {
   return (
     <>
-      <h1>Welcome to my new Charge site!</h1>
+      <h1>Monday Surf Club!</h1>
       <About />
     </>
   )
