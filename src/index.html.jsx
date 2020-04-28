02ba09b1692cda8c36040f698055f2b086e1bb92
seanwestfall <sean.westfall@gmail.com>
2020-04-28 04:15:35 -0700

    sw / update index with image

--- src/index.html.jsx
+++ src/index.html.jsx
@@ -4,6 +4,7 @@ export default () => {
   return (
     <main style={{textAlign: 'center'}}>
       <h1>Monday Morning Surf Club!</h1>
+      <img src="https://raw.githubusercontent.com/seanwestfall/mmsc/master/img/69e8f9e0475077548bc5830d1cbea1ee.jpg" alt="Surf" />
       <About />
     </main>
   )
