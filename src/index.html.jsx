50dc4bc8eee4aa056d211bf59816e4c93c344c59
seanwestfall <sean.westfall@gmail.com>
2020-04-28 04:17:29 -0700

    sw / changed the max width of the surf image

--- src/index.html.jsx
+++ src/index.html.jsx
@@ -4,7 +4,7 @@ export default () => {
   return (
     <main style={{textAlign: 'center'}}>
       <h1>Monday Morning Surf Club!</h1>
-      <img src="https://raw.githubusercontent.com/seanwestfall/mmsc/master/img/69e8f9e0475077548bc5830d1cbea1ee.jpg" alt="Surf" />
+      <img style={{maxWidth: '300px'}} src="https://raw.githubusercontent.com/seanwestfall/mmsc/master/img/69e8f9e0475077548bc5830d1cbea1ee.jpg" alt="Surf" />
       <About />
     </main>
   )
