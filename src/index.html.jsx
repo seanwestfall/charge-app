15027da2a522b40c2a7b4c95a6516e69ef13c074
seanwestfall <sean.westfall@gmail.com>
2020-04-28 23:54:22 -0700

    sw / update to the index page to include the index.css stylesheet

--- src/index.html.jsx
+++ src/index.html.jsx
@@ -2,10 +2,21 @@ import About from './pages/about.html.mdx'
 
 export default () => {
   return (
-    <main style={{textAlign: 'center'}}>
-      <h1>Monday Morning Surf Club!</h1>
-      <img style={{maxWidth: '300px'}} src="https://raw.githubusercontent.com/seanwestfall/mmsc/master/img/69e8f9e0475077548bc5830d1cbea1ee.jpg" alt="Surf" />
-      <About />
-    </main>
+    <html>
+    <head>
+      <title>Monday Morning Surf Club</title>
+
+      <link href="/stylesheets/index.css" rel="stylesheet" media="all" />
+      
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </head>
+    <body>
+      <main style={{textAlign: 'center'}}>
+        <h1>Monday Morning Surf Club!</h1>
+        <img style={{maxWidth: '300px'}} src="https://raw.githubusercontent.com/seanwestfall/mmsc/master/img/69e8f9e0475077548bc5830d1cbea1ee.jpg" alt="Surf" />
+        <About />
+      </main>
+    </body>
+    </html>
   )
 }
